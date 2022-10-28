26e46d3b007589ea1be035add51e748aec79edde
mrAJAY1 <thisismeajayprakash@gmail.com>
2022-10-28 11:05:17 +0530

    changed view to table

--- src/subComponents/copies/DataDisplay.jsx
+++ src/subComponents/copies/DataDisplay.jsx
@@ -1,59 +1,83 @@
 import React from "react";
-import EntryByPremiumCopy from "./EntryByPremiumCopy";
-import ExpiryKind from "./ExpiryKind";
-import Lots from "./Lots";
-import OptionType from "./OptionType";
-import Position from "./Position";
-import Strike from "./Strike";
-import ByStraddle from "./ByStraddle";
-import PremiumRange from "./EntryByPremiumRangeCopy";
-
+import styled from "styled-components";
+const Container = styled.div`
+  display: block;
+  margin: 50px 0px 35px 0;
+  th {
+    margin: 0px 15px 0px 15px;
+  }
+  td {
+    text-align: center;
+  }
+  tr:not(:first-child) {
+    margin-top: 20px;
+  }
+`;
 const DataDisplay = ({ values, index }) => {
   return (
-    <div style={{ display: "block", margin: "50px 0px 35px 0" }}>
-      <h3>{index + 1}.</h3> <br />
-      <h6>Lots : {values.Lots}</h6>
-      <br />
-      <h6>Position : {values.PositionType}</h6>
-      <br />
-      <h6>OptionType : {values.OptionType}</h6>
-      <br />
-      <h6>ExpiryKind : {values.ExpiryKind}</h6>
-      <br />
-      <h6>EntryType : {values.EntryType}</h6>
-      <br />
-      {values.EntryType === "EntryByPremium" ? (
-        <h6>Premium : {values.StrikeParameter}</h6>
-      ) : values.EntryType === "EntryByPremiumRange" ? (
-        <h6>
-          Premium Range : &#160; &#160; Lower: &#160; &#160;
-          {values.StrikeParameter.Lower} &#160; Upper: &#160; &#160;
-          {values.StrikeParameter.Upper}
-        </h6>
-      ) : values.EntryType === "EntryByStrikeType" ? (
-        <h6>Strike : {values.StrikeParameter}</h6>
-      ) : values.EntryType === "EntryByStraddlePrice" ? (
-        <h6>
-          StraddlePrice :
-          {`${values.StrikeParameter.Adjustment === "Plus" ? "  " : " - "} ${
-            values.StrikeParameter.Multiplier
-          }`}
-        </h6>
-      ) : (
-        <></>
-      )}
-      <br />
-      <h6>
-        Momentum : &#160;&#160; {values.LegMomentum.Type} &#160;&#160;
-        {values.LegMomentum.Value}
-      </h6>
-      <br />
-      <h6>
-        Trail SL : &#160;&#160; {values.LegTrailSL.Type} &#160;&#160;
-        {values.LegTrailSL.InstrumentMove}&#160;&#160;
-        {values.LegTrailSL.StopLossMove}
-      </h6>
-    </div>
+    <Container>
+      <table style={{ width: "100%" }}>
+        <tr>
+          <th>index</th>
+          <th>Lots</th>
+          <th>Position</th>
+          <th>Option</th>
+          <th>Expiry Kind</th>
+          <th>Entry Type</th>
+          <th>
+            {values.EntryType === "EntryByPremium"
+              ? "Premium"
+              : values.EntryType === "EntryByPremiumRange"
+              ? "Premium Range"
+              : values.EntryType === "EntryByStrikeType"
+              ? "Strike"
+              : values.EntryType === "EntryByStraddlePrice"
+              ? "StraddlePrice"
+              : ""}
+          </th>
+          {values.LegMomentum.Type !== "none" && <th>Momentum</th>}
+
+          {values.LegTrailSL.Type !== "none" && <th>TrailSL</th>}
+        </tr>
+        <tr>
+          <td>{index + 1}.</td>
+          <td> {values.Lots}</td>
+          <td> {values.PositionType}</td>
+          <td> {values.OptionType}</td>
+          <td> {values.ExpiryKind}</td>
+          <td>{values.EntryType}</td>
+          <td>
+            {values.EntryType === "EntryByPremium" ? (
+              values.StrikeParameter
+            ) : values.EntryType === "EntryByPremiumRange" ? (
+              (values.StrikeParameter.Lower, values.StrikeParameter.Upper)
+            ) : values.EntryType === "EntryByStrikeType" ? (
+              values.StrikeParameter
+            ) : values.EntryType === "EntryByStraddlePrice" ? (
+              (values.StrikeParameter.Adjustment === "Plus" ? "  " : " - ",
+              values.StrikeParameter.Multiplier)
+            ) : (
+              <></>
+            )}
+          </td>
+          {values.LegMomentum.Type !== "none" && (
+            <>
+              {" "}
+              <td>{values.LegMomentum.Type}</td>
+              <td>{values.LegMomentum.Value}</td>
+            </>
+          )}
+          {values.LegTrailSL.Type !== "none" && (
+            <>
+              {" "}
+              <td>{values.LegTrailSL.Type}</td>
+              <td>{values.LegTrailSL.InstrumentMove}</td>
+              <td>{values.LegTrailSL.StopLossMove}</td>
+            </>
+          )}
+        </tr>
+      </table>
+    </Container>
   );
 };
 
